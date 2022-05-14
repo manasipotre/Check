ad8df97990c678b200f009414ac56e959ae2b633
manasipotre <65325434+manasipotre@users.noreply.github.com>
2022-05-14 08:30:59 +0530

    Add files via upload

---
+++ Web Application Developement/5/index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app= express();
+app.use(express.static("public"));
+app.get("/",(req,res)=> {res.render ("index.html")})
+app.listen(8080,()=>{console.log("sender Started")})
